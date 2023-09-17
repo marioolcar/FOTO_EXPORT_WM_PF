e0ea5b704102a89f77a96a6ea0db1c6d8498237d
Mario Olčar <47773816+marioolcar@users.noreply.github.com>
2023-09-17 11:10:27 +0200

    uređivanje i dodavanje

--- plavifilter.lrplugin/info.lua
+++
@@ -1,7 +0,0 @@
-return {
-    LrSdkVersion = 6.0,
-    LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'dolje_uprava',
-    LrPluginName = "Plavi filter",
-    LrPluginInfoProvider = "test.lua"
-}
